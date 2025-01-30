f7ce3ebb465d0338a66cbc4709e0ba356066c874
Lukas Schwab <lukas.schwab@gmail.com>
2025-01-30 00:12:12 -0800

    Totally flatten structure

--- nilinterface.go
+++ nilinterface.go
@@ -2,8 +2,10 @@
 package linters
 
 import (
+	"go/ast"
+	"go/types"
+
 	"github.com/golangci/plugin-module-register/register"
-	"github.com/lukasschwab/nilinterface/pkg/nilinterface"
 	"golang.org/x/tools/go/analysis"
 )
 
@@ -18,9 +20,43 @@ func New(settings any) (register.LinterPlugin, error) {
 type linterPlugin struct{}
 
 func (l linterPlugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
-	return []*analysis.Analyzer{nilinterface.Analyzer}, nil
+	return []*analysis.Analyzer{Analyzer}, nil
 }
 
 func (l linterPlugin) GetLoadMode() string {
 	return register.LoadModeTypesInfo
+}
+
+var Analyzer = &analysis.Analyzer{
+	Name: "nilinterface",
+	Doc:  "check for nil passed to interface parameters",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			callExpr, ok := n.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+
+			for i, arg := range callExpr.Args {
+				if ident, ok := arg.(*ast.Ident); ok && ident.Name == "nil" {
+					if sig, ok := pass.TypesInfo.TypeOf(callExpr.Fun).(*types.Signature); ok {
+						if i < sig.Params().Len() && isInterface(sig.Params().At(i).Type()) {
+							pass.Reportf(arg.Pos(), "nil passed to interface parameter")
+						}
+					}
+				}
+			}
+			return true
+		})
+	}
+	return nil, nil
+}
+
+func isInterface(t types.Type) bool {
+	_, ok := t.Underlying().(*types.Interface)
+	return ok
 }
---
+++ nilinterface_test.go
@@ -0,0 +1,14 @@
+package linters_test
+
+import (
+	"testing"
+
+	linters "github.com/lukasschwab/nilinterface"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func TestAnalyzer(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, linters.Analyzer, "a")
+	analysistest.Run(t, testdata, linters.Analyzer, "b")
+}
